87ad8ff6a8a0d18c5b88359172ad27949a5620f9
LC <64722907+lc6464@users.noreply.github.com>
2024-06-16 00:56:58 +0800

    添加代码

---
+++ README.md
@@ -0,0 +1,21 @@
+# MyTan 补全计划
+
+## 项目简介
+MyTan 补全计划是一个基于篡改猴（Tampermonkey）的脚本项目，旨在为 MyTan 网页端提供更多的快捷键功能，提升用户体验和操作效率。
+
+## 功能特性
+- 为一些常用操作添加快捷键支持
+- 自动更新
+
+## 安装指南
+1. 安装 [Tampermonkey](https://www.tampermonkey.net/) 浏览器扩展。
+2. [点击此处](https://lc6464.github.io/MyTan-Enhancer/shortcut.user.js) 安装 MyTan 补全计划脚本。
+3. 安装完成后，刷新 MyTan 网页，脚本将自动生效。
+
+## 新增快捷键列表
+- Alt + N：新建对话
+- F2：重命名对话
+- 在重命名对话时按 Enter：保存新的对话名称
+
+## 许可证
+本项目采用 MIT 许可证。详细信息请参阅 [LICENSE](LICENSE) 文件。
---
+++ shortcut.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         MyTan 补全计划：快捷键
+// @namespace    https://lcwebsite.cn/
+// @version      0.1.0
+// @description  通过新增一些快捷键让 MyTan Web 端更好用！
+// @author       LC
+// @match        https://mytan.maiseed.com.cn/*
+// @icon         https://mytan.maiseed.com.cn/assets/single-logo.png
+// @grant        none
+// ==/UserScript==
+
+(function() {
+	'use strict';
+
+	if (location.pathname.startsWith('/chat')) {
+		// 聊天页面
+		document.body.addEventListener('keydown', e => {
+			if (e.key === "n" && e.altKey) {
+				// Alt + N：新建对话
+				e.preventDefault();
+				document.querySelector('.create').click();
+				return;
+			}
+
+			if (e.key === "F2" && location.pathname !== "/chat" && location.pathname !== "/chat/undefined") {
+				// F2：重命名对话
+				e.preventDefault();
+				document.querySelector('svg-icon[key="edit"]').click();
+				return;
+			}
+
+			if (e.key === "Enter" && e.target !== null && e.target === document.querySelector('.title-input')) {
+				// 在重命名对话时按 Enter：保存新的对话名称
+				e.preventDefault();
+				document.querySelector('[nztype="primary"]').click();
+				return;
+			}
+		});
+	}
+})();
